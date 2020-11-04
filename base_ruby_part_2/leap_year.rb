e5bdb955267f6cae44cc76f54cff8d970147321d
Kiselev <mogikan232323@gmail.com>
2020-11-04 22:14:05 +0300

    simple logic

--- base_ruby_part_2/leap_year.rb
+++ base_ruby_part_2/leap_year.rb
@@ -1,36 +1,13 @@
-require 'colorize'
-
-TEXT = {
-    height_error: 'Invalid number. Insert correct number'.red,
-}
-
 def get_value(value)
-  loop do
-    puts "Insert #{value}:"
-    result = gets.chomp
-    return result.to_i if check_int?(result)
-  end
-end
-
-def check_int?(int)
-  unless int[/^\d+$/]
-    puts TEXT[:height_error]
-    return false
-  end
-  true
-end
-
-def leap_year?(year)
-  (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)
-end
-
-def yday(day, month, year)
-  day_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  day_of_month[1] += 1 if leap_year?(year)
-  day_of_month.take(month - 1).sum + day
+  puts "Insert #{value}:"
+  gets.chomp.to_i
 end
 
 day = get_value('day')
 month = get_value('month')
 year = get_value('year')
-puts "Order number of date: #{yday(day, month, year)}"
+
+day_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_of_month[1] += 1 if (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)
+
+puts "Order number of date: #{day_of_month.take(month - 1).sum + day}"
