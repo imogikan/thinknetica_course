73f893d62ace5812929e139bbcad4ea3c2fc51d3
Kiselev <mogikan232323@gmail.com>
2020-11-04 22:04:41 +0300

    each.with_index

--- base_ruby_part_2/letters.rb
+++ base_ruby_part_2/letters.rb
@@ -2,8 +2,8 @@ require 'pry'
 require 'pp'
 
 vowel_letters_with_order_number = {}
-("a".."z").each_with_index do |letter, index|
-  vowel_letters_with_order_number[letter] = index + 1 if letter[/[aeoui]/]
+("a".."z").each.with_index(1) do |letter, index|
+  vowel_letters_with_order_number[letter] = index if letter[/[aeoui]/]
 end
 
 pp vowel_letters_with_order_number
